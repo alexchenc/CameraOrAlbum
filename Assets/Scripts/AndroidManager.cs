f0a5a7ef8d79a93328728016e30ae1e109813279
AlexChen <474857292@qq.com>
2018-06-01 23:15:45 +0800

    调用android打开相册/相机

---
+++ Assets/Scripts/AndroidManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    /// <summary>
+    /// 处理Unity与Android交互
+    /// </summary>
+    public class AndroidManager : MonoBehaviour
+    {
+        public ChangeHead changeHead;
+
+        void LogDebug(string str)
+        {
+            //Debug.Log(str);
+        }
+
+        void LogError(string str)
+        {
+            //Debug.Error(str);
+        }
+
+        public void GetImage(string imagePath)
+        {
+            Debug.Log("Android get image callback." + imagePath);
+            changeHead.GetImage(imagePath);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ChangeHead.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHead : MonoBehaviour
+{
+    public const string FILE_PREFIX = "file://";
+
+    public UITexture headTexture;
+    public GameObject btnAlbum;
+    public GameObject btnPhoto;
+
+    private void Awake()
+    {
+        UIEventListener.Get(btnAlbum).onClick += OpenAlbum;
+        UIEventListener.Get(btnPhoto).onClick += OpenPhoto;
+    }
+
+    void OpenAlbum(GameObject go)
+    {
+#if UNITY_EDITOR
+        //nothing
+#else
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("openAlbum");
+#endif
+    }
+
+    void OpenPhoto(GameObject go)
+    {
+#if UNITY_EDITOR
+        //nothing
+#else
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("openCamera");
+#endif
+    }
+
+    /// <summary>
+    /// 选择完图片回调
+    /// </summary>
+    /// <param name="data">Data.</param>
+    public void GetImage(object data)
+    {
+        const string TEMP_IMAGE = "/images/temp.png";
+        StartCoroutine(LoadTextureFromLocal(TEMP_IMAGE, delegate (Texture texture)
+        {
+            //设置图片
+            headTexture.mainTexture = texture;
+        }));
+    }
+
+    /// <summary>
+    /// 读取本地图片
+    /// </summary>
+    /// <returns>The texture.</returns>
+    /// <param name="relativePath">Path.</param>
+    /// <param name="callback">Callback.</param>
+    public IEnumerator LoadTextureFromLocal(string relativePath, Action<Texture> callback)
+    {
+        if (!string.IsNullOrEmpty(relativePath) && callback != null)
+        {
+            //从本地获取，file://xxx
+            string localPath = FILE_PREFIX + Application.persistentDataPath + relativePath;
+            Debug.Log("LoadTexture at local " + localPath);
+
+            WWW www = new WWW(localPath);
+            yield return www;
+            if (www.error != null)
+            {
+                Debug.LogError("加载本地图片失败;" + www.error);
+                callback(null);
+            }
+            else
+            {
+                callback(www.texture);
+            }
+        }
+    }
+
+}
